4a10f8938bbe4c22d766b4b89d56832eddeb25ab
matthewot <616539757@qq.com>
2019-11-23 17:46:00 +0800

    日常

--- src/main/java/com/gtmdtest/platform/controller/MainController.java
+++ src/main/java/com/gtmdtest/platform/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.gtmdtest.platform.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @program: platform
@@ -16,6 +18,21 @@ public class MainController {
     public String hello(){
         return "html/index";
     }
+
+    @GetMapping("listPage")
+    public String listPage(@RequestParam(value = "pageType",required = false) String pageType,
+                           Model model){
+        model.addAttribute("pageType",pageType);
+        return "html/listpage";
+    }
+
+    @GetMapping("/apiPage")
+    public String apiPage(){
+        return "html/apipage";
+    }
+
+
+
 
     @GetMapping("/errorPage")
     public String errorPage(){
