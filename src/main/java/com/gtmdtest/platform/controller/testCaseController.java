a00be57114218e10b3a9f2d6d6f2c1629fb8afe8
matthewot <616539757@qq.com>
2019-11-30 14:31:44 +0800

    日常

--- src/main/java/com/gtmdtest/platform/controller/testCaseController.java
+++ src/main/java/com/gtmdtest/platform/controller/testCaseController.java
@@ -8,7 +8,9 @@ import com.gtmdtest.platform.service.testCaseService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: platform
@@ -20,27 +22,60 @@ import java.util.List;
 @RestController
 public class testCaseController {
 
+    private static final Map<String,String> SYSTEM_NAME = new HashMap(){{
+        put("eSaler","电销");
+        put("sunflower","直销");
+        put("publicService","公共服务");
+        put("agency","代理商");
+        put("finance","财务");
+        put("afterMarket","车后服务");
+    }};
+
     @Resource
     testCaseService testCaseService;
 
     @PostMapping("/add")
-    public BaseResponseVO addTestCase(){
-        return null;
+    public BaseResponseVO addTestCase(@RequestParam(value = "systemType")Integer systemType,
+                                      @RequestParam(value = "caseName")String caseName,
+                                      @RequestParam(value = "descption")String descption,
+                                      @RequestParam(value = "runUrl")String runUrl){
+        TestCase testCase = new TestCase();
+        testCase.setCaseName(caseName);
+        testCase.setSystemType(systemType);
+        testCase.setSystemName(SYSTEM_NAME.get(systemType));
+        testCase.setDescription(descption);
+        testCase.setRunUrl(runUrl);
+        testCaseService.addTestCase(testCase);
+        return BaseResponseVO.success("成功");
     }
 
     @GetMapping("/deleteById")
-    public BaseResponseVO deleteTestCase(){
-        return null;
+    public BaseResponseVO deleteTestCase(@RequestParam(value = "caseId")Integer caseId){
+        testCaseService.deleteTestCase(caseId);
+        return BaseResponseVO.success("操作成功");
     }
 
     @PostMapping("/update")
-    public BaseResponseVO updateTestCase(){
-        return null;
+    public BaseResponseVO updateTestCase(@RequestParam(value ="id" )Integer id,
+                                         @RequestParam(value = "systemType")Integer systemType,
+                                         @RequestParam(value = "caseName")String caseName,
+                                         @RequestParam(value = "descption")String descption,
+                                         @RequestParam(value = "runUrl")String runUrl){
+        TestCase testCase = new TestCase();
+        testCase.setId(id);
+        testCase.setCaseName(caseName);
+        testCase.setSystemType(systemType);
+        testCase.setSystemName(SYSTEM_NAME.get(systemType));
+        testCase.setDescription(descption);
+        testCase.setRunUrl(runUrl);
+        testCaseService.updateTestCase(testCase);
+        return BaseResponseVO.success("成功");
     }
 
     @GetMapping("/getById")
-    public BaseResponseVO getTestCaseById(){
-        return null;
+    public BaseResponseVO getTestCaseById(@RequestParam(value = "caseId")Integer caseId){
+
+        return BaseResponseVO.success(testCaseService.getTestCaseById(caseId));
     }
 
     @GetMapping("/getAll")
